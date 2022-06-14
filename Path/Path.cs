c46fba532f0486ea96eb488377e3ad655e724e5b
Alisa Borkowska <alisa.borkowska@gmail.com>
2022-06-14 06:31:59 +0200

    feat: error handling

--- Path/Path.cs
+++ Path/Path.cs
@@ -1,5 +1,9 @@
 namespace TrafficController.Path;
 
+public class InvalidRouteError: Exception {
+    public InvalidRouteError(): base("Invalid route provided!") {}
+}
+
 public struct Path
 {
     public readonly Queue<char> Route;
@@ -8,6 +12,7 @@ public struct Path
 
     public Path(int velocity, int height, Queue<char> route)
     {
+        if (route.Count( (char c) => { return "NSWE".Contains(c.ToString().ToUpper()); }) != route.Count) throw new InvalidRouteError();
         Velocity = velocity;
         Height = height;
         Route = route;
